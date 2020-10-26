92a0400f592a2ce7f9e9c463d6e1ae2cb298bb13
Luis <tennis.bresil@gmail.com>
2020-10-26 13:53:37 -0400

    Amelhoracao display card

--- view/film/index.php
+++ view/film/index.php
@@ -1,8 +1,7 @@
 <?php include '../../includes/head.php'; ?>
 <?php include '../../includes/interfaceVisiteur.php'; ?>
 
-<div class="container-fluid">   
-
+<div class="container">  
     <div class="row mb-5">
               <!--  ICONE + TITLE -->
             <div class="col-md-8">
@@ -17,21 +16,12 @@
                           class="form-control" 
                           placeholder="# ID du film">  -->
             </div> 
-    </div>   
+    </div> 
+    <!--  RENDER PAGE -->
+         <div class="flex-container" id="listTemplate">
+           <!-- CHARGE LE TEMPLATE(cards filme) ICI !--> 
 
-      <!--  RENDER PAGE --> 
-    <div class="row">
-        <div class="col-md-3">
-
-             <div  id="listTemplate">
-               <!-- CHARGE LE TEMPLATE(cards filme) ICI !-->           
-             </div>
-
-        </div>
-    </div>
-    
-    
+         </div>       
 </div>  
-
 
 <?php include '../../includes/footer.php'; ?>
