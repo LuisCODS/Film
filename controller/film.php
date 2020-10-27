a4ee6c6b9da8c0f76814ab57034a06df3fe53a12
Luis <tennis.bresil@gmail.com>
2020-10-26 21:48:36 -0400

    Correction display card

--- controller/film.php
+++ controller/film.php
@@ -16,38 +16,20 @@ extract($_POST);
 	switch ($action) 
 	{
 		case 'insert':
+			$dossier="";
+			$nomPochette=sha1($titre.time());
+			$pochette="avatar.jpg";
 			
-			$dossierPath="../img";
-			//$dossierPath = $_SERVER['DOCUMENT_ROOT'].'\\FilmPHP__MVC_GIT\img\\'.$pochette;
-			//$dossierPath= dirname(__FILE__).'\\FilmPHP__MVC_GIT\\img\\';					
-			//$dossierPath="../img/";  //Serveur path
-			$nomPochette=sha1($titre.time()); //Create a tolk key unique(new name)	
-			
-			$pochette="avatar.jpg";//Default image si pas de img fournit
-			//var_dump($dossierPath);
-			//print_r($dossierPath);
-
-				//Si une image a été envoyéé
-				if($_FILES['pochette']['tmp_name']!=="")
-				{
-					//Get real name file from server side
-					$filename = $_FILES['pochette']['tmp_name'];
-
-					//Get name file from user side
-					$fichier= $_FILES['pochette']['name'];
-
-					// Get file extension 
-					$extension=strrchr($fichier,'.');
-
-					//Move file to  server path:  filename/img/nomPochette.jpg
-					@move_uploaded_file($filename,$dossierPath.$nomPochette.$extension);//le @ enleve les msn d'erreur cote client
-					
-					//effacer le fichier temporaire
-					@unlink($filename); 
-
-					$pochette=$nomPochette.$extension;//Attache le nom  generé coté serveur avec l'extention obtenu cote client
-				}
-
+			if($_FILES['pochette']['tmp_name']!==""){
+				//Upload de la photo
+				$tmp = $_FILES['pochette']['tmp_name'];
+				$fichier= $_FILES['pochette']['name'];
+				$extension=strrchr($fichier,'.');
+				@move_uploaded_file($tmp,$dossier.$nomPochette.$extension);
+				// Enlever le fichier temporaire chargé
+				@unlink($tmp); //effacer le fichier temporaire
+				$pochette=$nomPochette.$extension;
+			}
 				$film = new Film(null,$titre,$prix,$realisateur,$categorie,$pochette,$description);	
 				$filmDAO->insert($film);//Si ok return 1
 				//location.reload(true);
